4e88863f898c6a7cff8c77335cc0fc805b10bc34
Brandon Orther <brandon@orther.dev>
2021-03-17 06:24:47 -0700

    Clean up import since unused value isn't needed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-import LensGuide from "./lensGuide";
+import "./lensGuide";
 
 // -----------------------
 // -- Render App to DOM --
